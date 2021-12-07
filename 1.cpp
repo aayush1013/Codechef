b56072365b828ae836075bbf00e5eba389eddb33
Aayush Mittal <aayush102003@gmail.com>
2021-12-07 16:23:05 +0530

    program 1

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+int n1,n2;
+cin>>n1>>n2;
+if (n1>n2)
+{
+    cout<<'>'<<endl;
+}
+else if (n1==n2)
+{
+    cout<<'='<<endl;
+}
+else 
+{
+    cout<<'<'<<endl;
+}
+    }
+	return 0;
+}
