6322ff186f9d9f0253269d4756db9a12c28def61
Aayush Mittal <aayush102003@gmail.com>
2021-12-18 21:32:24 +0530

    DIFFSUM

---
+++ 7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int N1,N2;
+cin>>N1>>N2;
+if (N1>N2)
+{
+   int Difference=N1-N2;
+    cout<<Difference<<endl;
+}
+else
+{
+    int sum = N1+N2;
+    cout<<sum<<endl;
+}
+	return 0;
+}
