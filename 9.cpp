166337ea783fcd003db05516d9c4a9a88c26a25f
Aayush Mittal <aayush102003@gmail.com>
2021-12-19 22:58:16 +0530

    random ques

---
+++ 9.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i;
+    {
+        if (i==1)
+    {
+        cout<<"1";
+    }
+}
+if (i>1)
+{
+    cout<<i-1;
+    for (int a=1;a<=i-2;a++)
+    {
+        cout<<"0";
+    }
+    cout<<i-1;
+}
+cout<<"\n";
+}
