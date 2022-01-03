22e06c229ae61e5be7a648d5cac5614d282b2390
Aayush Mittal <aayush102003@gmail.com>
2022-01-03 14:30:47 +0530

    greedy puppy

---
+++ 26.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    int x;
+    {
+        x=n%k;
+        cout<<x<<endl;
+    }
+}
+	return 0;
+}
