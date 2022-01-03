9e2238b83dce440119ebf348c415fb42642d7d69
Aayush Mittal <aayush102003@gmail.com>
2022-01-03 15:21:16 +0530

    updated greedy puppy

--- 26.cpp
+++ 26.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 int main() {
-int t;
-cin>>t;
-while(t--){
-    int n;
-    cin>>n;
-    int k;
-    cin>>k;
-    int x;
-    {
-        x=n%k;
-        cout<<x<<endl;
-    }
-}
+	// your code goes here
+	int t=0;
+	cin>>t;
+	for(;t>0;t--){
+	    int n=0,k=0;
+	    cin>>n>>k;
+	    int max=0;
+	    for(int i=1;i<=k;i++){
+	        int temp=n%i;
+	        if(temp>max){
+	            max=temp;
+	        }
+	    }
+	    cout<<max<<endl;
+	}
 	return 0;
 }
