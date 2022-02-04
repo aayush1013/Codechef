05efb843de3d0bef399a3d2b6499803e960f079c
Aayush Mittal <aayush102003@gmail.com>
2022-02-04 16:49:58 +0530

    chef and the hair salon

---
+++ 34.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N,M;
+        cin>>N>>M;
+        int pro;
+        pro = N*M;
+        cout<<pro<<" ";
+    }
+	// your code goes here
+	return 0;
+}
